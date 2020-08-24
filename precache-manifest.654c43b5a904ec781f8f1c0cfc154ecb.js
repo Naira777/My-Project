fe879d9f5fce66e5e16521ad931d6c6680729e62
Naira777 <sp_naira@yahoo.com>
2020-08-24 15:05:16 +0400

    Updates

---
+++ precache-manifest.654c43b5a904ec781f8f1c0cfc154ecb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16d1791da2309a16ac426d2a98103aa",
+    "url": "/My-project/index.html"
+  },
+  {
+    "revision": "1951ad3442a8454b601f",
+    "url": "/My-project/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0b3286cd7f9033222981",
+    "url": "/My-project/static/css/main.8c29eb3c.chunk.css"
+  },
+  {
+    "revision": "1951ad3442a8454b601f",
+    "url": "/My-project/static/js/2.4ba3a307.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-project/static/js/2.4ba3a307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b3286cd7f9033222981",
+    "url": "/My-project/static/js/main.0027f7cc.chunk.js"
+  },
+  {
+    "revision": "ae654fa0fcff6366cb25",
+    "url": "/My-project/static/js/runtime-main.9731e6ac.js"
+  },
+  {
+    "revision": "efc3857811ea0813316475b79af16c7c",
+    "url": "/My-project/static/media/earth.efc38578.jpg"
+  },
+  {
+    "revision": "cb68c6dc19a868d5a71902da3265e9dd",
+    "url": "/My-project/static/media/earth2.cb68c6dc.jpg"
+  }
+]);
